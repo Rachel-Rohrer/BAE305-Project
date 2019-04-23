1de4e68c2ddd1c5c0911537357480a1c5c8456b9
AbbyOlaleye <46763280+AbbyOlaleye@users.noreply.github.com>
2019-04-23 19:52:51 -0400

    Add files via upload

---
+++ 305_Display_Screen.ino
@@ -0,0 +1,144 @@
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <SPI.h>
+
+#ifdef ADAFRUIT_HALLOWING
+  #define TFT_CS        39 // Hallowing display control pins: chip select
+  #define TFT_RST       37 // Display reset
+  #define TFT_DC        38 // Display data/command select
+  #define TFT_BACKLIGHT  7 // Display backlight pin
+#else
+  // For the breakout board, you can use any 2 or 3 pins.
+  // These pins will also work for the 1.8" TFT shield.
+  #define TFT_CS        10
+  #define TFT_RST        9 // Or set to -1 and connect to Arduino RESET pin
+  #define TFT_DC         8
+#endif
+
+
+// For 1.44" and 1.8" TFT with ST7735 (including HalloWing) use:
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+
+float p = 3.1415926;
+
+const int ACC = A1;     //the accelerometer is connected to pin A1
+float threshold = 400;  //threshold the sensor must surpass to activate reply
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.print(F("Hello! ST77xx TFT Test"));
+
+#ifdef ADAFRUIT_HALLOWING
+  // HalloWing is a special case. It uses a ST7735R display just like the
+  // breakout board, but the orientation and backlight control are different.
+  tft.initR(INITR_HALLOWING);        // Initialize HalloWing-oriented screen
+  pinMode(TFT_BACKLIGHT, OUTPUT);
+  digitalWrite(TFT_BACKLIGHT, HIGH); // Backlight on
+#else
+  // Use this initializer if using a 1.8" TFT screen:
+  tft.initR(INITR_144GREENTAB);      // Init ST7735R chip, green tab
+
+#endif
+
+  uint16_t time = millis();
+  tft.fillScreen(ST77XX_BLACK);
+  time = millis() - time;
+
+
+  String ReplyArray[13] = {"It is certain.", "It is decidedly so.", "Withough a doubt.", "Most likely.", "Outlook good.", "Yes.","Reply hazy, try again.", "Ask again later.", "Cannot predict now.", "Don't count on it.", "My reply is no.", "Very doubtful", "Ask Dr. Dvorak"};                      //the array of possible replies
+        
+        float sensorValueAccelerometer = analogRead(A1); //naming a variable for what the sensor is reading
+        Serial.println (sensorValueAccelerometer);       //printing what the sensor is reading
+        String Reply = ReplyArray[random(12)];           //assigning "Reply" variable to be random choice from array
+        if (sensorValueAccelerometer > threshold){
+          Serial.println (Reply);                        //printing "Reply" if sensor reads beyond threshold
+        
+        }
+        delay (1000);                                    //delay for testing
+        
+
+  // large block of text
+  testdrawtext(Reply, ST77XX_WHITE);
+  delay(1000);
+  }
+
+
+ 
+
+void loop() {
+  tft.invertDisplay(true);
+  delay(500);
+  tft.invertDisplay(false);
+  delay(500);
+
+  }
+
+void testdrawtext(String text, uint16_t color) {
+  tft.setCursor(0, 0);
+  tft.setTextColor(color);
+  tft.setTextWrap(true);
+  tft.print(text);
+
+}
+
+void tftPrintTest() {
+  tft.setTextWrap(false);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(0, 30);
+  tft.setTextColor(ST77XX_RED);
+  tft.setTextSize(1);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setTextSize(2);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_GREEN);
+  tft.setTextSize(3);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_BLUE);
+  tft.setTextSize(4);
+  tft.print(1234.567);
+  delay(1500);
+  tft.setCursor(0, 0);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(0);
+  tft.println("Hello World!");
+  tft.setTextSize(1);
+  tft.setTextColor(ST77XX_GREEN);
+  tft.print(p, 6);
+  tft.println(" Want pi?");
+  tft.println(" ");
+  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
+  tft.println(" Print HEX!");
+  tft.println(" ");
+  tft.setTextColor(ST77XX_WHITE);
+  tft.println("Sketch has been");
+  tft.println("running for: ");
+  tft.setTextColor(ST77XX_MAGENTA);
+  tft.print(millis() / 1000);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.print(" seconds.");
+}
+
+void mediabuttons() {
+  // play
+  tft.fillScreen(ST77XX_BLACK);
+  tft.fillRoundRect(25, 10, 78, 60, 8, ST77XX_WHITE);
+  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST77XX_RED);
+  delay(500);
+  // pause
+  tft.fillRoundRect(25, 90, 78, 60, 8, ST77XX_WHITE);
+  tft.fillRoundRect(39, 98, 20, 45, 5, ST77XX_GREEN);
+  tft.fillRoundRect(69, 98, 20, 45, 5, ST77XX_GREEN);
+  delay(500);
+  // play color
+  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST77XX_BLUE);
+  delay(50);
+  // pause color
+  tft.fillRoundRect(39, 98, 20, 45, 5, ST77XX_RED);
+  tft.fillRoundRect(69, 98, 20, 45, 5, ST77XX_RED);
+  // play color
+  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST77XX_GREEN);
+}
